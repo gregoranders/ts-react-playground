074c79fe0837c20e0d0c10988737ef2ffb2cf5cb
Gregor Anders <gregor.anders@gmail.com>
2020-06-12 03:55:32 +0200

    #21 - add missing file

---
+++ workbox-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  globDirectory: 'public/',
+  globPatterns: ['**/*.{html,txt,js,css,json,webapp,ico,xml,png}'],
+  swDest: 'public/serviceWorker.js',
+  swSrc: 'public/serviceWorker.js',
+};
