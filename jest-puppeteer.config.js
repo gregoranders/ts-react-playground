f122a6b0ecf14a571e8ef2fd0a10b2abe6d7ec5f
Gregor Anders <gregor.anders@gmail.com>
2020-06-20 02:18:07 +0200

    build(ssl): fix build on macos
    fix build on macos
    
    #55

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -8,9 +8,9 @@ const command = `cross-env NODE_PORT=${port} NODE_ENV=production npm run ${proce
 const args = ['--enable-experimental-web-platform-features'];
 
 // Did not manage to get self signed certs working on macos
-if (process.platform === 'darwin') {
-  args.push('--allow-insecure-localhost');
-}
+// if (process.platform === 'darwin') {
+//   args.push('--allow-insecure-localhost');
+// }
 
 module.exports = {
   browser: 'chromium',
