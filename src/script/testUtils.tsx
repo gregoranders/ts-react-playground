b83155dd552bc6f8809ba115d255c4c1407f89bb
Gregor Anders <gregor.anders@gmail.com>
2020-06-22 22:17:17 +0200

    fix(test): fix test

--- src/script/testUtils.tsx
+++ src/script/testUtils.tsx
@@ -2,12 +2,14 @@ jest.useFakeTimers();
 
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
+import { act } from 'react-dom/test-utils';
 
 import 'jest-enzyme';
 
 import { RecoilRoot } from 'recoil';
-const { act, create } = TestRenderer;
 import { mount, render } from 'enzyme';
+
+const { create } = TestRenderer;
 
 export { RecoilRoot, act, create, mount, render };
 
